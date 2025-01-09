211e71973cc9a4b12b0d661b04bd95178b17236c
atul <theatulparihar1081@gmail.com>
2025-01-09 15:39:17 +0530

    Check login

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -17,7 +17,7 @@ export const AuthProvider = ({ children }) => {
   const login = async (email, password) => {
     try {
       console.log(process.env.REACT_APP_BACKEND_URL);
-      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/auth/login`, { email, password });
+      const response = await axios.post(`/api/auth/login`, { email, password });
 
       const { token, user } = response.data;
       localStorage.setItem('authToken', token);
